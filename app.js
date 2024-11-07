ba3a02923923dd5f462e44e03d4de3c5e96a6213
Kekhayev Rodion <Kekhayev.r@mail.ru>
2024-11-07 22:22:49 +0500

    jenkins test commit

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, Node.js!');
+    const message = '<h1>API is working, don`t worry be happy</h1>'
+    res.end(message);
 });
 
 const PORT = 3000;
 server.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
 });
+
+
+
 
 function sendMessage(){
     const message = 'API is working, don`t worry be happy'
